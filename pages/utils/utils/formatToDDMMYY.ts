a274d162c3a7afebc1346ad612ac7bfcf84a9d27
tomrot <46992492+tomrot@users.noreply.github.com>
2025-06-12 00:39:51 +0300

    Delete pages/utils/utils directory

--- pages/utils/utils/formatToDDMMYY.ts
+++
@@ -1,7 +0,0 @@
-export default function formatToDDMMYY(dateStr: string): string {
-  const d = new Date(dateStr);
-  const dd = (`0${d.getDate()}`).slice(-2);
-  const mm = (`0${d.getMonth()+1}`).slice(-2);
-  const yy = String(d.getFullYear()).slice(-2);
-  return `${dd}${mm}${yy}`;
-}
