f5b39668cf01879491e41ec0e3016ba2c7692b4b
tomrot <46992492+tomrot@users.noreply.github.com>
2025-06-12 01:17:47 +0300

    Create _app.tsx

---
+++ pages/_app.tsx
@@ -0,0 +1,14 @@
+// pages/_app.tsx
+import '../styles/globals.css';
+import Head from 'next/head';
+
+export default function App({ Component, pageProps }) {
+  return (
+    <>
+      <Head>
+        <script src="https://cdn.tailwindcss.com"></script>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
+}
