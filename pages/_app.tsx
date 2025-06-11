3d3a99028b07cfae07b3b3d6c9d76ed3c54d3767
tomrot <46992492+tomrot@users.noreply.github.com>
2025-06-12 01:28:03 +0300

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,13 @@
 // pages/_app.tsx
 import '../styles/globals.css';
+import type { AppProps } from 'next/app';
 import Head from 'next/head';
 
-export default function App({ Component, pageProps }) {
+export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
+        <!-- Tailwind Play CDN for styling without local build -->
         <script src="https://cdn.tailwindcss.com"></script>
       </Head>
       <Component {...pageProps} />
